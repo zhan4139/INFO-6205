9ff60dbec226477bbff23393e62e45a6940a6632
Tingyuan Zhang <zhan4139@umn.edu>
2017-09-10 14:38:34 -0700

    finished n_2 sort

--- Lecture1.java
+++ Lecture1.java
@@ -4,14 +4,15 @@ class Lecture1 {
 	public static void main (String[] args) {
 		int[] arr = {10, 5, 8, 6, 3, 7, 5};
 
-		new Lecture1().BubbleSort2(arr);
+		//new Lecture1().BubbleSort2(arr);
+		new Lecture1().InsertionSort(arr);
 		System.out.println(Arrays.toString(arr));
 		//System.out.println(arr.length);
 
 	}
 
 
-
+	//Lecture version
 	public void BubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
@@ -35,6 +36,40 @@ class Lecture1 {
 		}
 	}
 
+	public void SelectionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[min])
+					min = j;
+			}
+			if (min != i) 
+				Swap(arr, i, min);
+			//System.out.println(arr[i]);
+		}
+	}
+
+	public void InsertionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int j = i + 1;
+			int temp = arr[j];
+			while (j > 0 && temp < arr[j-1]) {
+				//Swap(arr, j-1, j);
+				arr[j] = arr[j-1];
+				j--;
+			}
+			arr[j] = temp;
+		}
+
+	}
+
+	public void MergeSort(int[] arr) {
+
+	}
+
+	public void QuickSort(int[] arr) {
+
+	}
 	//helper function
 	private void Swap(int[] arr, int first, int second) {
 		int temp = arr[first];
