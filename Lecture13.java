e5c4a7b4a69e0980bd38d02e0a42215f85cc1d7e
zhan4139 <zhan4139@umn.edu>
2017-12-03 19:56:08 -0800

    working

---
+++ Lecture13.java
@@ -0,0 +1,32 @@
+class Lecture13 {
+	//Final Exam content
+	//1. 
+	//create a class, whenever it inserts a value, it becomes
+	//sorted linkedlist and it is circular
+	//function: insert(if it is null, point to itself, dummy node required)
+	
+
+	//2. tree, another(subtree), check if mirror of subtree exists in the tree
+	//could be part of the tree instead of strictly whole part, return true
+	
+
+	//3. Flood Fill, horizental and vertical and diagonal 
+	// change the connected 1 to change value (only the connected 1s)
+
+
+
+	//Graph (MST)
+	//Kruskal's Algo
+	// 1)sort based on weight
+	// 2)
+	
+	//Boriuvka's Algo
+	// 1)find the minimum weight associated to the node
+	// 2)join the set
+	
+
+	//Prim's Algo
+	//
+
+	
+}
